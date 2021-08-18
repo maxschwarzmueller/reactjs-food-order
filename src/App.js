817671521e2c88088fc72370a14c32ded44a0977
Manuel Lorenz <manuellorenz@Academinds-Mini.fritz.box>
2021-08-18 11:37:18 +0200

    added cart modal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
+import Cart from './components/Cart/Cart';
 
 function App() {
   return (
     <CartProvider>
+      <Cart />
       <Header />
       <main>
         <Meals />
